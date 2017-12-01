c25f1fbbb4848fbc94d7b69d386b46fd8de17a06
takatoh <takatoh.m@gmail.com>
2017-12-02 05:26:00 +0900

    main.go: Cleaning.

--- main.go
+++ main.go
@@ -25,25 +25,20 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Not MAG format")
 		os.Exit(0)
 	}
-//	fmt.Println("MAG format")
 
 	mag.MachineCode(magfile)
-//	fmt.Println(machineCode)
 	user := mag.User(magfile)
-//	fmt.Println(user)
 	comment := mag.Comment(magfile)
-//	fmt.Println(comment)
-
 	header := mag.ReadHeader(magfile)
 
 	if *opt_information {
 		fmt.Printf("user=%s\n", user)
 		fmt.Printf("comment=%s\n", comment)
 		fmt.Printf("colors=%d\n", header.Colors)
-//		fmt.Println(header.StartX, header.StartY, header.EndX, header.EndY)
 		fmt.Printf("width=%d, height=%d\n", header.Width, header.Height)
 		fmt.Printf("FlagA: offset=%d size=%d\n", header.FlgAOffset, header.FlgASize)
 		fmt.Printf("FlagB: offset=%d size=%d\n", header.FlgBOffset, header.FlgBSize)
 		fmt.Printf("Pixel: offset=%d size=%d\n", header.PxOffset, header.PxSize)
+		os.Exit(0)
 	}
 }
