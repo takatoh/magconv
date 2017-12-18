c11b13e8c6a9438ce3c9f90b43d630fd0b6d7206
takatoh <takatoh.m@gmail.com>
2017-12-18 11:42:29 +0900

    main.go: Support wild card for filename.

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"image/color"
 	"image/png"
 	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/takatoh/magconv/mag"
@@ -46,79 +47,89 @@ Options:
 		os.Exit(0)
 	}
 
-	filename := flag.Args()[0]
-	magfile, err := os.Open(filename)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Cannot open file: %s\n", filename)
-		os.Exit(1)
-	}
-	defer magfile.Close()
-
 	loader := mag.NewLoader()
 
-	loader.Init(magfile)
-	check := loader.CheckMag
-	if !check {
-		fmt.Fprintln(os.Stderr, "Not MAG format")
-		os.Exit(0)
-	}
+	pattern := flag.Args()[0]
+//	fmt.Println(pattern)
+	filenames, _ := filepath.Glob(pattern)
+//	fmt.Println(filenames)
 
-	loader.Load()
+	for _, filename := range filenames {
+//	filename := flag.Args()[0]
 
-	if *opt_info {
-		header := loader.Header
-		fmt.Printf("user=%s\n", loader.User)
-		fmt.Printf("comment=%s\n", loader.Comment)
-		fmt.Printf("colors=%d\n", header.Colors)
-		fmt.Printf("width=%d, height=%d\n", header.Width, header.Height)
-		fmt.Printf("FlagA: offset=%d size=%d\n", header.FlgAOffset, header.FlgASize)
-		fmt.Printf("FlagB: offset=%d size=%d\n", header.FlgBOffset, header.FlgBSize)
-		fmt.Printf("Pixel: offset=%d size=%d\n", header.PxOffset, header.PxSize)
+//		fmt.Println(filename)
 
-		os.Exit(0)
-	}
-
-	if *opt_palettes {
-		palettes := loader.Palettes
-		fmt.Println("Palettes:")
-		for i, palette := range palettes {
-			fmt.Printf("%d: r=%02x, g=%02x, b=%02x\n", i, palette.R, palette.G, palette.B)
+		magfile, err := os.Open(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Cannot open file: %s\n", filename)
+			os.Exit(1)
 		}
-		os.Exit(0)
-	}
+		defer magfile.Close()
 
-	if *opt_flags {
-		printFlag(loader.FlagA, "Flag A")
-		printFlag(loader.FlagB, "Flag B")
-		os.Exit(0)
-	}
-
-	if *opt_pixels {
-		printFlag(loader.Pixel, "Pixels")
-		os.Exit(0)
-	}
-
-	result := loader.Expand()
-
-	w := int(loader.Header.Width)
-	h := int(loader.Header.Height)
-	img := image.NewRGBA(image.Rect(0, 0, w, h))
-	for x := 0; x < w; x++ {
-		for y := 0; y < h; y++ {
-			r := result[y][x].R * 16
-			g := result[y][x].G * 16
-			b := result[y][x].B * 16
-			c := color.RGBA{r, g, b, 255}
-			img.Set(x, y, c)
+		loader.Init(magfile)
+		check := loader.CheckMag
+		if !check {
+			fmt.Fprintln(os.Stderr, "Not MAG format")
+			os.Exit(0)
 		}
-	}
 
-	ext := path.Ext(filename)
-	pngFilename := strings.Replace(filename, ext, ".png", 1)
-	f, err := os.OpenFile(pngFilename, os.O_WRONLY|os.O_CREATE, 0600)
-	if err != nil {
-		fmt.Println(err)
+		loader.Load()
+
+		if *opt_info {
+			header := loader.Header
+			fmt.Printf("user=%s\n", loader.User)
+			fmt.Printf("comment=%s\n", loader.Comment)
+			fmt.Printf("colors=%d\n", header.Colors)
+			fmt.Printf("width=%d, height=%d\n", header.Width, header.Height)
+			fmt.Printf("FlagA: offset=%d size=%d\n", header.FlgAOffset, header.FlgASize)
+			fmt.Printf("FlagB: offset=%d size=%d\n", header.FlgBOffset, header.FlgBSize)
+			fmt.Printf("Pixel: offset=%d size=%d\n", header.PxOffset, header.PxSize)
+
+			os.Exit(0)
+		}
+
+		if *opt_palettes {
+			palettes := loader.Palettes
+			fmt.Println("Palettes:")
+			for i, palette := range palettes {
+				fmt.Printf("%d: r=%02x, g=%02x, b=%02x\n", i, palette.R, palette.G, palette.B)
+			}
+			os.Exit(0)
+		}
+
+		if *opt_flags {
+			printFlag(loader.FlagA, "Flag A")
+			printFlag(loader.FlagB, "Flag B")
+			os.Exit(0)
+		}
+
+		if *opt_pixels {
+			printFlag(loader.Pixel, "Pixels")
+			os.Exit(0)
+		}
+
+		result := loader.Expand()
+
+		w := int(loader.Header.Width)
+		h := int(loader.Header.Height)
+		img := image.NewRGBA(image.Rect(0, 0, w, h))
+		for x := 0; x < w; x++ {
+			for y := 0; y < h; y++ {
+				r := result[y][x].R * 16
+				g := result[y][x].G * 16
+				b := result[y][x].B * 16
+				c := color.RGBA{r, g, b, 255}
+				img.Set(x, y, c)
+			}
+		}
+
+		ext := path.Ext(filename)
+		pngFilename := strings.Replace(filename, ext, ".png", 1)
+		f, err := os.OpenFile(pngFilename, os.O_WRONLY|os.O_CREATE, 0600)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer f.Close()
+		png.Encode(f, img)
 	}
-	defer f.Close()
-	png.Encode(f, img)
 }
